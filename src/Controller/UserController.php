99e0e5292304da28fabd2a7730fb4f45405713e3
Claire <clpereira@outlook.fr>
2025-05-28 17:54:03 +0200

    commit final

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractController
@@ -30,6 +31,7 @@ class UserController extends AbstractController
 
         foreach ($users as $user) {
             $birthdays = [];
+
             foreach ($user->getBirthdays() as $birthday) {
                 $birthdays[] = [
                     'id' => $birthday->getId(),
@@ -51,16 +53,16 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="app_user_new", methods={"POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
         $user = new User();
         $user->setEmail($data['email']);
         $user->setRoles($data['roles'] ?? ['ROLE_USER']);
-        $user->setPassword($data['password']);
-        // $user->setName($data['name']);
-        // $user->setBirthday(new \DateTime($data['birthday']));
+
+        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
 
         $entityManager->persist($user);
         $entityManager->flush();
@@ -71,45 +73,25 @@ class UserController extends AbstractController
     /**
      * @Route("/user/{id}", name="app_user_show", methods={"GET"})
      */
-    public function show(User $user): JsonResponse
+    public function show(Request $request, EntityManagerInterface $entityManager): JsonResponse
     {
-        $data = [
+        $user = $entityManager->getRepository(User::class)->find($request->get('id'));
+
+        if (!$user) {
+            return $this->json(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        // Check if the user is the owner of the resource
+        // if ($user->getId() !== $this->getUser()->getId()) {
+        //     return $this->json(['error' => 'Access denied'], Response::HTTP_FORBIDDEN);
+        // }
+
+        return $this->json([
             'id' => $user->getId(),
             'email' => $user->getEmail(),
-            'password' => $user->getPassword(),
             'roles' => $user->getRoles(),
-            // 'birthday' => $user->getBirthday()->format('Y-m-d'),
-        ];
-
-        return $this->json($data);
-    }
-
-    /**
-     * @Route("/user/{id}", name="app_user_edit", methods={"PUT"})
-     */
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        // $user->setName($data['name']);
-        $user->setEmail($data['email']);
-        $user->setPassword($data['password'] ?? $user->getPassword());
-        $user->setRoles($data['roles'] ?? $user->getRoles());
-        // $user->setBirthday(new \DateTime($data['birthday']));
-
-        $entityManager->flush();
-
-        return $this->json(['message' => 'User updated successfully']);
-    }
-
-    /**
-     * @Route("/user/{id}", name="app_user_delete", methods={"DELETE"})
-     */
-    public function delete(User $user, EntityManagerInterface $entityManager): JsonResponse
-    {
-        $entityManager->remove($user);
-        $entityManager->flush();
-
-        return $this->json(['message' => 'User deleted successfully']);
+            // 'name' => $user->getName(),
+            // 'birthday' => $user->getBirthday() ? $user->getBirthday()->format('Y-m-d') : null,
+        ]);
     }
 }
